803de6d6c2e6ef3e6d548c65df9b0896100afcc8
lindsmw <mlindsay@smu.edu>
2018-07-31 10:49:55 -0500

    *imgs not loading* pushing before design change

--- personalwebsite/src/Components/Experience.js
+++ personalwebsite/src/Components/Experience.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { Card, CardContent, CardMedia, CardHeader, Typography, GridList, GridListTile, Paper } from '@material-ui/core';
 
+
 const style = {
-	card: {
+	jobcard: {
 		maxWidth:700,
-		// height: 400,
+		height: 300,
 		margin: 35,
 		padding: 20,
 		display: 'inline-block',
 		background:'#9E9E9E',
 	},
+	projectcard: {
+		maxWidth: 300,
+		height: 'auto',
+		margin: 35,
+		padding: 20,
+		display: 'inline-block'
+	},
 	detail: {
 		display: 'flex'
 	},
 	img: {
-		width: 400,
+		width: 300,
 		height: 'auto'
 	},
 	paper: {
@@ -26,6 +34,13 @@ const style = {
 	},
 	display2: {
 		padding: 10,
+		textAlign: 'center',
+		borderBottom: '1px solid black',
+		marginTop: 20
+	},
+	media: {
+		width: 400,
+		height: 'auto'
 	}
 }
 
@@ -42,25 +57,28 @@ class Experience extends Component {
 			  	<Typography variant="display2" component="h1" style={style.display2}>
           			Work Experience
         		</Typography>
-				<Card className="jobcard" style={style.card}>
+				<Card className="jobcard" style={style.jobcard}>
 					<div style={style.detail} >
 					<CardMedia>
-						<img style={style.img} src="./stockmusic.png" alt="stockmusic logo" />
+						<img style={style.img} src="./favicon.ico" alt="stockmusic logo" />
 					</CardMedia>
 					<CardContent style={style.content}>
 						<Typography variant="headline" component="h1">
 						Software Engineer Intern, Summer 2017
 						</Typography>
 						<Typography variant="body1" >
-						Description
+						Stockmusic.net is a royalty-free music platform where thousands of users go to purchase music for commercials, business presentations, and those alike. 
+						Working primarily on the backend, I wrote many PHP artisan commands that served the purpose of shortening the workload to access and manipulate data in 
+						the database. This included commands to automatically transcode original music with the watermark tracks, retrieve details of a specific track, 
+						and retrieve customer information all with certain specifications.
 						</Typography>
 					</CardContent>
 					</div>
 				</Card>
-				<Card className="jobcard" style={style.card}>
+				<Card className="jobcard" style={style.jobcard}>
 					<div style={style.detail} >
 					<CardMedia>
-						<img style={style.img} src="./microsoft.png" alt="microsoft logo" />
+						<img style={style.img} src="./favicon.ico" alt="microsoft logo" />
 					</CardMedia>
 					<CardContent>
 						<Typography variant="headline" component="h1">
@@ -84,6 +102,29 @@ class Experience extends Component {
 				</Card>*/}
 				{/*</GridListTile>
 			</GridList>*/}
+				<Typography variant="display2" component="h1" style={style.display2}>
+					Projects
+				</Typography>
+				<Card className="projectcard" style={style.projectcard}>
+					<CardMedia>
+						<img style={style.img} src="./favicon.ico" alt="project logo" />
+					</CardMedia>
+					<CardContent>
+						<Typography variant="headline" component="h1">
+						Choir Connect Web Application
+						</Typography>
+					</CardContent>
+				</Card>
+				<Card className="projectcard" style={style.projectcard}>
+					<CardMedia>
+						<img style={style.img} src="./favicon.ico" alt="project logo" />
+					</CardMedia>
+					<CardContent>
+						<Typography variant="headline" component="h1">
+						NinjaTrader Automation Software
+						</Typography>
+					</CardContent>
+				</Card>
 			</Paper>
 			</div>
 		)
