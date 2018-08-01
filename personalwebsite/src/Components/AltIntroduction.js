6e79e095918f90ad5d3a22240e9d415531e26f16
lindsmw <mlindsay@smu.edu>
2018-07-31 23:39:14 -0500

    testing

--- personalwebsite/src/Components/AltIntroduction.js
+++ personalwebsite/src/Components/AltIntroduction.js
@@ -37,7 +37,7 @@ class AltIntroduction extends Component {
     				</Typography>*/}
     			</div>
     			<div className="rightHalf" >
-    				
+    				<p>hello</p>
     			</div>
     			{/*</Paper>*/}
 			</div>
